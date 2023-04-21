bcc1d89d3f027924f0f17345dac81134f150d0fd
Wellington Feijó <wellingtonfc95@gmail.com>
2023-04-21 01:38:37 -0300

    Ajuste tecnico list, criação do model

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
   templateUrl: './tecnico-list.component.html',
   styleUrls: ['./tecnico-list.component.css']
 })
-export class TecnicoListComponent {
+export class TecnicoListComponent implements OnInit {
 
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Valdir',
+      cpf: '123.432.123-32',
+      email: 'valdir@email.com',
+      senha: '1234',
+      perfis: ['0'],
+      dataCriacao: '21/04/2023'
+    }
+  ]
+
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+ 
+  constructor(){}
+
+  ngOnInit(): void {
+   
+  }
+
+  
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
+
+
+
+
